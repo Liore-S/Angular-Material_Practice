4dc6abf4dc9db9bd4d62760cd88070c75a605aac
Liore-S <goober395@gmail.com>
2021-03-18 05:21:31 +0700

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { SidebarComponent } from './dashboard/sidebar/sidebar.component';
+import { HeaderComponent } from './dashboard/header/header.component';
+import { MainComponent } from './dashboard/main/main.component';
+import { EmailComponent } from './dashboard/main/email/email.component';
+import { AccountComponent } from './dashboard/main/account/account.component';
+import { GameComponent } from './dashboard/main/game/game.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//material
+import { MatCardModule } from '@angular/material/card'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    SidebarComponent,
+    HeaderComponent,
+    MainComponent,
+    EmailComponent,
+    AccountComponent,
+    GameComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
